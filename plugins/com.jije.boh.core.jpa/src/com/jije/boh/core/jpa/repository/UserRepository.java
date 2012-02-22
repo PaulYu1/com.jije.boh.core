483ef0bd58ae99d185e0e1e51054ec516e9a94df
Murphy <murphylan@hotmail.com>
2012-02-22 21:38:32 +0800

    add jpa

--- plugins/com.jije.boh.core.jpa/src/com/jije/boh/core/jpa/repository/UserRepository.java
+++ plugins/com.jije.boh.core.jpa/src/com/jije/boh/core/jpa/repository/UserRepository.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.jije.boh.core.jpa.domain.User;
 
@@ -16,7 +16,7 @@ import com.jije.boh.core.jpa.domain.User;
  * 
  * @author Murphy
  */
-public interface UserRepository extends CrudRepository<User, Long>{
+public interface UserRepository extends PagingAndSortingRepository<User, Long>{
 
     List<User> findByLastname(String lastname);
 
