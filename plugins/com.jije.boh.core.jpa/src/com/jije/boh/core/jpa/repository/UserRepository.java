5c12e8d509551c618e5682ea81e594e283b7138d
Murphy <murphylan@hotmail.com>
2012-02-23 09:18:02 +0800

    update jpa demo

--- plugins/com.jije.boh.core.jpa/src/com/jije/boh/core/jpa/repository/UserRepository.java
+++ plugins/com.jije.boh.core.jpa/src/com/jije/boh/core/jpa/repository/UserRepository.java
@@ -4,19 +4,23 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jije.boh.core.jpa.domain.User;
 
-
 /**
- * Repository interface for {@link User} instances. Provides basic CRUD operations due
- * to the extension of {@link JpaRepository}. Includes custom implemented
- * functionality by extending {@link UserRepositoryCustom}.
+ * Repository interface for {@link User} instances. Provides basic CRUD
+ * operations due to the extension of {@link JpaRepository}. Includes custom
+ * implemented functionality by extending {@link UserRepositoryCustom}.
  * 
  * @author Murphy
  */
-public interface UserRepository extends CrudRepository<User, Long>{
+@Transactional
+public interface UserRepository extends CrudRepository<User, Long> {
 
-    List<User> findByLastname(String lastname);
+	@Transactional(readOnly = true)
+	List<User> findByLastname(String lastname);
 
+	@Transactional(readOnly = true)
+	long count();
 }
--- plugins/com.jije.boh.core.service/src/com/jije/boh/core/service/OperateService.java
+++ plugins/com.jije.boh.core.service/src/com/jije/boh/core/service/OperateService.java
@@ -1,32 +1,20 @@
 package com.jije.boh.core.service;
 
 import java.util.Date;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceReference;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.jije.boh.core.jpa.repository.UserRepository;
 import com.jije.boh.core.service.inter.IOperateService;
 
 @Component("operateService")
 public class OperateService implements IOperateService {
-	private CrudRepository _crudRepository;
 
-	public CrudRepository getCrudRepository() {
-		BundleContext context = Activator.getContext();
-		ServiceReference<?> serviceReference = context
-				.getServiceReference(CrudRepository.class.getName());
-		CrudRepository dao = (CrudRepository) context
-				.getService(serviceReference);
-		return dao;
-	}
-
-	OperateService() {
-		this._crudRepository = getCrudRepository();
-	}
+	@Autowired
+	private UserRepository userDao;
 
 	public long getCount() {
-		return _crudRepository.count();
+		return userDao.count();
 	}
 
 	public String getDate() {
